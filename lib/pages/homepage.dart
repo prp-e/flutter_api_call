12b0d114cdb1e2321ce7affa84a54d6624be9f32
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-14 15:37:38 +0330

    updated: homepage

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -10,6 +10,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
   }
 }
