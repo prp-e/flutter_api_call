33caa60dfa5f36a576f6747af3d3495ab1b3744b
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-12-14 15:38:33 +0330

    changed: primary color

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.red,
       ),
       home: const HomePage(),
     );
